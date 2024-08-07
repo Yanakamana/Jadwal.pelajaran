3ea81203eb5cb9e0ed2922384a64f7170490b4dc
yana <tayoempus@gmail.com>
2024-08-07 09:46:48 +0700

    update

---
+++ main.js
@@ -0,0 +1,92 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBm9HdJ92vSLrKRclI6Z2J4bmvlFgR4AuU",
+  authDomain: "mang-yana.firebaseapp.com",
+  projectId: "mang-yana",
+  storageBucket: "mang-yana.appspot.com",
+  messagingSenderId: "1094982396668",
+  appId: "1:1094982396668:web:2d103526ab40a59efc0579",
+  measurementId: "G-MF48P7VG5P"
+};
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function ambilDaftarAbsensi() {
+  const refDokumen = collection(db, "absensi");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikanKueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      hari: dok.data().hari,
+      jamke: dok.data().jamke,
+      kelas: dok.data().kelas,
+      mapel: dok.data().mapel,
+      namaguru: dok.data().namaguru,
+      waktu: dok.data().waktu,
+
+    });
+  });
+
+
+
+  return hasil;
+}
+
+export function formatAngka(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
+
+export async function tambahAbsensi(tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
+  try {
+    const dokRef = await addDoc(collection(db, 'absensi'), {
+      hari: hari,
+      jamke: jamke,
+      kelas: kelas,
+      mapel: mapel,
+      namaguru: namaguru,
+      waktu: waktu,
+    });
+    console.log('berhasil menembah ' + dokRef.id);
+  } catch (e) {
+    console.log('gagal menambah ' + e);
+  }
+}
+
+export async function hapusAbsensi(docId) {
+  await deleteDoc(doc(db, "absensi", docId));
+}
+
+export async function ubahAbsensi(docId, tanggal, nis, nama, alamat, noTlpn, kelas, keterangan) {
+  await updateDoc(doc(db, "absensi", docId), {
+    hari: hari,
+      jamke: jamke,
+      kelas: kelas,
+      mapel: mapel,
+      namaguru: namaguru,
+      waktu: waktu,
+  });
+}
+
+export async function ambilAbsensi(docId) {
+  const docRef = await doc(db, "absensi", docId);
+  const docSnap = await getDoc(docRef);
+
+  return await docSnap.data();
+}
